f27a24e68b53f18564c8d498b45d13a5cc06e1d8
NguyenThanhVinh2711 <vinh99657@gmail.com>
2022-12-07 15:18:48 +0700

    update song

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AddSongComponent } from './song/add-song/add-song.component';
+import { SongserviceComponent } from './service/songservice/songservice.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddSongComponent,
+    SongserviceComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/model/song.ts
@@ -0,0 +1,13 @@
+export interface Song {
+  id?: any;
+  name?: any;
+  describeSong?: any;
+  fileMp3?: any;
+  avatar?: any;
+  author?: any;
+  singer?: any;
+  nameCreate?: any;
+  songTypeSet?: any;
+  album?: any;
+  view?: any;
+}
