e3f3a1ef57db8e25e646d4792082b347492d769b
HanPham <petphamhandc@gmail.com>
2022-12-08 15:52:48 +0700

    add edit Song

--- src/app/song/edit-song/edit-song.component.ts
+++ src/app/song/edit-song/edit-song.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, ParamMap} from "@angular/router";
+import {FormControl, FormGroup} from "@angular/forms";
+import {SongService} from "../../service/song.service";
 
 @Component({
-  selector: 'app-edit-song',
-  templateUrl: './edit-song.component.html',
-  styleUrls: ['./edit-song.component.css']
+  selector: 'app-song-edit',
+  templateUrl: './song-edit.component.html',
+  styleUrls: ['./song-edit.component.css']
 })
-export class EditSongComponent {
+export class EditSongComponent implements OnInit {
+// @ts-ignore
+  id: number;
+  // @ts-ignore
+  songForm: FormGroup;
 
+  constructor(private songService: SongService, private activateRoute: ActivatedRoute) {
+    this.activateRoute.paramMap.subscribe((paraMap: ParamMap) => {
+      // @ts-ignore
+      this.id = +paraMap.get('id');
+      this.getSong(this.id);
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  getSong(id: number) {
+    return this.songService.getById(id).subscribe(song => {
+      this.songForm = new FormGroup({
+        id: new FormControl(song.id),
+        title: new FormControl(song.title),
+        price: new FormControl(song.price),
+        description: new FormControl(song.description)
+      })
+    })
+  }
+
+  editSong() {
+    const editSong = this.songForm.value
+    this.songService.editSong(editSong.id, editSong).subscribe(() => {
+      alert("Cập nhập thành công");
+    })
+  }
 }
