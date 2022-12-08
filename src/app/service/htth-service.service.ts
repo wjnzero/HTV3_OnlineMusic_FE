8e59e74248442c0e01755d8861aa07a0f60391ea
HanPham <petphamhandc@gmail.com>
2022-12-08 11:09:15 +0700

    test playlist service

---
+++ src/app/service/htth-service.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {HttpHeaders} from '@angular/common/http';
+
+// @ts-ignore
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  token = sessionStorage.getItem('auth-token');
+  // tslint:disable-next-line:variable-name
+  headers_object = new HttpHeaders().set('Authorization', 'Bearer ' + this.token);
+  httpOptions = {
+    headers: this.headers_object
+  };
+  // @ts-ignore
+  id: string;
+
+  constructor() { }
+
+  // tslint:disable-next-line:contextual-lifecycle use-lifecycle-interface
+  ngOnInit(): void {
+  }
+
+  // tslint:disable-next-line:typedef
+  getHttp(){
+    return this.httpOptions;
+  }
+
+  // tslint:disable-next-line:typedef
+  getID(): string {
+    // @ts-ignore
+    return this.idUser = sessionStorage.getItem('id_User');
+  }
+}
---
+++ src/app/service/playlist.service.ts
@@ -0,0 +1,64 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {environment} from "../../enronments/environment";
+import {HttpService} from "./htth-service.service";
+import {Observable} from "rxjs";
+import {Playlist} from "../model/playlist";
+
+const API_URL = `${environment.apiUrl}`;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlaylistService {
+  constructor(private http: HttpClient,
+              private httpService: HttpService) {
+  }
+
+  getAllPlaylists(): Observable<Playlist[]> {
+    return this.http.get<Playlist[]>(API_URL + '/home/playlist');
+  }
+
+  getAllPlaylistsNew(): Observable<Playlist[]> {
+    return this.http.get<Playlist[]>(API_URL + '/home/playlist/newest');
+  }
+
+
+  // Lấy toàn bộ bài hát trong playlist theo Id
+  getPlaylistById(idPlaylist: number): Observable<Playlist> {
+    return this.http.get<Playlist>(API_URL + '/home/playlist/' + idPlaylist);
+  }
+
+  getPlaylistByName(search: string): Observable<Playlist[]> {
+    let params = new HttpParams().set('search', search);
+    return this.http.get<Playlist[]>(API_URL + '/home/playlist/search', {params});
+  }
+
+  getPlaylistByUser(idUser: number): Observable<Playlist[]> {
+    // @ts-ignore
+    return this.http.get<Playlist[]>(API_URL + '/home/playlist/list/' + idUser, this.httpService.getHttp());
+  }
+
+  createPlaylist(playlist: Playlist): Observable<any> {
+    return this.http.post(API_URL + '/playlist', playlist, this.httpService.getHttp());
+  }
+
+  updatePlaylist(playlist: Playlist): Observable<any> {
+    return this.http.put(API_URL + '/playlist/update', playlist, this.httpService.getHttp());
+  }
+
+  updateSongOfPlaylist(idPlaylist: number, idSong: number): Observable<any> {
+    // @ts-ignore
+    return this.http.get(API_URL + `/home/song/${idSong}/${idPlaylist}`, this.httpService.getHttp());
+  }
+
+  // xóa playlist
+  deletePlaylist(idPlaylist: number): Observable<any> {
+    return this.http.delete(API_URL + '/home/playlist/' + idPlaylist, this.httpService.getHttp());
+  }
+
+  // Xóa bài hát khỏi playlist
+  deleteSongOfPlaylist(idPlaylist: number, idSong: number): Observable<any> {
+    return this.http.delete(API_URL + `/home/song/${idSong}/${idPlaylist}`, this.httpService.getHttp());
+  }
+}
