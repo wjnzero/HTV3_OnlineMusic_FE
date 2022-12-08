1cc465277c981d43e74441b38a801f90a10432c3
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-12-08 15:51:52 +0700

    8/12

--- src/app/song/delete/delete.component.spec.ts
+++ src/app/song/delete/delete.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DeleteSongComponent } from './delete-song.component';
+import { DeleteComponent } from './delete.component';
 
-describe('DeleteSongComponent', () => {
-  let component: DeleteSongComponent;
-  let fixture: ComponentFixture<DeleteSongComponent>;
+describe('DeleteComponent', () => {
+  let component: DeleteComponent;
+  let fixture: ComponentFixture<DeleteComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DeleteSongComponent ]
+      declarations: [ DeleteComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(DeleteSongComponent);
+    fixture = TestBed.createComponent(DeleteComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
