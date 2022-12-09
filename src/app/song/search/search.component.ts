eab26c15b166762cd55572979e5377020e215cc0
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-12-09 13:33:32 +0700

    9/12

--- src/app/song/search/search.component.ts
+++ src/app/song/search/search.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Song} from "../../model/song";
 import {ActivatedRoute, ParamMap} from "@angular/router";
 import {SongService} from "../../service/song.service";
+import {PlaylistService} from "../../service/playlist.service";
+import {Playlist} from "../../model/playlist";
 
 @Component({
   selector: 'app-search',
@@ -10,27 +12,49 @@ import {SongService} from "../../service/song.service";
 })
 export class SearchComponent implements OnInit{
   // @ts-ignore
-  songs: Song;
+  // songs: Song;
+  //
+  // constructor(
+  //   private activateRoute: ActivatedRoute,
+  //   private songService: SongService) {
+  // }
+  //
+  // ngOnInit():void{
+  //   this.songs = {
+  //     name: '', describeSong: '', avatar: '',author: '', singer: '', album: '', view:''}
+  //
+  //   this.activateRoute.paramMap.subscribe((paraMap: ParamMap) => {
+  //     const id = paraMap.get('id');
+  //     this.getByName(id);
+  //   });
+  // }
+  //
+  // getByName(id: any) {
+  //   this.songService.getById(id).subscribe((yy: Song) => {
+  //     this.songs = yy;
+  //   })
+  // }
 
-  constructor(
-    private activateRoute: ActivatedRoute,
-    private songService: SongService) {
-  }
+  search: string;
+  songLists: Song[] = [];
+  playLists: Playlist[] = [];
+  p: any;
+  page: any;
 
-  ngOnInit():void{
-    this.songs = {
-      name: '', describeSong: '', avatar: '',author: '', singer: '', album: '', view:''}
+  constructor(private songService: SongService,
+              private playlistService: PlaylistService,
+              private activatedRoute: ActivatedRoute) { }
 
-    this.activateRoute.paramMap.subscribe((paraMap: ParamMap) => {
-      const id = paraMap.get('id');
-      this.getByName(id);
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe(params => {
+      this.search = params['name'];
+      this.songService.getByName(this.search).subscribe((res: Song[]) => {
+        this.songLists = res;
+      });
+      this.playlistService.getPlaylistByName(this.search).subscribe(res => {
+        this.playLists = res;
+      });
     });
-  }
-
-  getByName(id: any) {
-    this.songService.getById(id).subscribe((yy: Song) => {
-      this.songs = yy;
-    })
   }
 }
 
--- src/app/song/song.module.ts
+++ src/app/song/song.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { SongRoutingModule } from './song-routing.module';
 import {ListSongComponent} from "./list-song/list-song.component";
 import {ReactiveFormsModule} from "@angular/forms";
+import { PlayerComponent } from './player/player.component';
 
 
 @NgModule({
   declarations: [
-    ListSongComponent
+    ListSongComponent,
+    PlayerComponent
   ],
   exports: [
     ListSongComponent
