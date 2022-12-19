1ccc7999d1913439e4c69f875be672dad9bf9139
xuanhieuvnhd <xuanhieuvn@gmail.com>
2022-12-19 12:40:58 +0700

    fix playlist all song

--- src/app/playlist/playlist-routing.module.ts
+++ src/app/playlist/playlist-routing.module.ts
@@ -5,6 +5,7 @@ import {NgModule} from "@angular/core";
 import {PlaylistComponent} from "./playlist/playlist.component";
 import {ListsongInOnePlaylistComponent} from "./listsong-in-one-playlist/listsong-in-one-playlist.component";
 import {AuthGuardGuard} from "../guard/auth-guard.guard";
+import {PlaySongComponent} from "../home/play-song/play-song.component";
 
 
 
@@ -27,6 +28,10 @@ const routes: Routes = [
     path: "findsonginplaylist/:id",
     component: ListsongInOnePlaylistComponent
   },
+  {
+    path: 'playlist/song/:id',
+    component:PlaySongComponent }
+
   // {
   //   path: "userplaylist/:id",
   //   component: PlaylistComponent,
